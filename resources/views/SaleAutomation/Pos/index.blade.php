8c85fea0f5778b515c6458e8a3d6e0369e85b4a1
Quardruple <jokeretaaye@gmail.com>
2019-08-21 17:46:13 +0700

    Update index.blade.php

--- resources/views/SaleAutomation/Pos/index.blade.php
+++ resources/views/SaleAutomation/Pos/index.blade.php
@@ -8,8 +8,21 @@
 <input type='hidden' id='SystemName' name='SystemName' value='<?php //echo $SystemName; ?>'>
 
 <div class='block-content'>
-    
+    <div class="row" style="height:300px;">
+        <div class="col-7" style="border:solid 1px black;">
+
+        </div>
+        <div class="col-5" style="border:solid 1px black;">
+                <div class="col-4">
+                    <input type='number' class="form-control" id='QtyBarcode' name='QtyBarcode' min='1' max='99' value='1'>
+                </div>
+                <div class="8">
+                        <input list='Goods' class="form-control" id='GoodsBarcodeSearch' autofocus>
+                </div>
+        </div>
+    </div>
 </div>
+
 @endsection
 <div class='ExtendsModal'>
     @extends('Shared.Modal.Goods.NoGoodsBarcode')
